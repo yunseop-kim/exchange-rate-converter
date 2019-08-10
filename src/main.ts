c8476ee3c3626e2b7d6449ba9025ef3c7b433fac
Yunseop Kim <undefined@mealant.com>
2019-08-10 17:11:13 +0900

    times 내에서

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 export class Dollar {
-  public amount: Number = 5 * 2;
+  public amount: Number;
   constructor(amount: Number) {
   }
   public times(multiplier: number): void {
+    this.amount = 5 * 2
   }
 }
