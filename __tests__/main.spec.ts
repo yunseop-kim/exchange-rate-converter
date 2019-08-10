891eb870742755d8fe60e8b9e60cbddcb6e2b614
Yunseop Kim <undefined@mealant.com>
2019-08-10 17:57:41 +0900

    인라인

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -3,11 +3,8 @@ import { Dollar } from '../src/main';
 describe('Dollar', () => {
   it('manipulation', () => {
     const five: Dollar = new Dollar(5);
-    let product: Dollar = five.times(2);
-    five.times(2);
-    expect(new Dollar(10)).toEqual(product);
-    product = five.times(3);
-    expect(new Dollar(15)).toEqual(product);
+    expect(new Dollar(10)).toEqual(five.times(2));
+    expect(new Dollar(15)).toEqual(five.times(3));
   });
 
   it('equality', () => {
