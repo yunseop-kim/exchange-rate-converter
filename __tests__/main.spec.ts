50d7379b68114f7c18f8d9ffdd232ed6a74b4b8c
Yunseop Kim <undefined@mealant.com>
2019-08-10 17:18:36 +0900

    실패

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -5,5 +5,7 @@ describe('Dollar', () => {
     const five: Dollar = new Dollar(5);
     five.times(2);
     expect(five.amount).toBe(10);
+    five.times(3);
+    expect(five.amount).toBe(15);
   });
 });
