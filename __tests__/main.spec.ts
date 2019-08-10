391923d74696861ebd57f0e14068c8f06298b7a8
Yunseop Kim <undefined@mealant.com>
2019-08-10 18:18:47 +0900

    Money 테스트를 뺌

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -6,13 +6,6 @@ describe('Dollar', () => {
     expect(new Dollar(10)).toEqual(five.times(2));
     expect(new Dollar(15)).toEqual(five.times(3));
   });
-
-  it('equality', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
-    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
-  });
 });
 
 describe('Franc', () => {
@@ -21,5 +14,13 @@ describe('Franc', () => {
     expect(new Franc(10)).toEqual(five.times(2));
     expect(new Franc(15)).toEqual(five.times(3));
   });
+});
 
+describe('Money', () => {
+  it('equality', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
+    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+  });
 });
